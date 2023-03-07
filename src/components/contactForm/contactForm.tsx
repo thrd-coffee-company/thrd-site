1561bbea2662f75b4185c72b103280a59ab8a746
Ozzy Cardenas <ozzycardenas@Ozzys-MacBook-Pro.local>
2023-03-06 22:12:38 -0600

    add netlify recaptcha

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -5,34 +5,34 @@ import {
   Input,
   Textarea,
   Stack,
-  Button,
-} from "@chakra-ui/react";
-import { Formik, Form, Field } from "formik";
-import { useRouter } from "next/router";
-import * as Yup from "yup";
+  Button
+} from '@chakra-ui/react';
+import { Formik, Form, Field } from 'formik';
+import { useRouter } from 'next/router';
+import * as Yup from 'yup';
 
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
   lastName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
   company: Yup.string(),
-  email: Yup.string().email("Invalid email").required("Required"),
-  phoneNumber: Yup.string().matches(phoneRegExp, "Invalid phone number"),
-  message: Yup.string().min(10, "Too Short!").required("Required"),
+  email: Yup.string().email('Invalid email').required('Required'),
+  phoneNumber: Yup.string().matches(phoneRegExp, 'Invalid phone number'),
+  message: Yup.string().min(10, 'Too Short!').required('Required')
 });
 
 const encode = (data) => {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 };
 
 const ContactForm = () => {
@@ -40,21 +40,21 @@ const ContactForm = () => {
   return (
     <Formik
       initialValues={{
-        firstName: "",
-        lastName: "",
-        company: "",
-        email: "",
-        phoneNumber: "",
-        message: "",
+        firstName: '',
+        lastName: '',
+        company: '',
+        email: '',
+        phoneNumber: '',
+        message: ''
       }}
       validationSchema={SignupSchema}
       onSubmit={(values, actions) => {
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "book-event", ...values }),
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: encode({ 'form-name': 'book-event', ...values })
         })
-          .then(() => router.push("/success"))
+          .then(() => router.push('/success'))
           .catch((error) => alert(error));
 
         actions.setSubmitting(false);
@@ -66,11 +66,12 @@ const ContactForm = () => {
           method="post"
           action="/success"
           data-netlify="true"
+          data-netlify-recaptcha="true"
           data-netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="book-event" />
           <Stack spacing={6}>
-            <Stack direction={{ base: "column", lg: "row" }} spacing={8}>
+            <Stack direction={{ base: 'column', lg: 'row' }} spacing={8}>
               <Field name="firstName">
                 {({ field, form }) => (
                   <FormControl
@@ -173,16 +174,19 @@ const ContactForm = () => {
               )}
             </Field>
           </Stack>
-          <Button
-            type="submit"
-            mt={4}
-            isLoading={props.isSubmitting}
-            bg="purple.600"
-            _hover={{ bg: "purple.700" }}
-            color="white"
-          >
-            Submit
-          </Button>
+          <Stack mt={4} direction='row'>
+            <Button
+              type="submit"
+              mr={4}
+              isLoading={props.isSubmitting}
+              bg="purple.600"
+              _hover={{ bg: 'purple.700' }}
+              color="white"
+            >
+              Submit
+            </Button>
+            <div data-netlify-recaptcha="true"></div>
+          </Stack>
         </Form>
       )}
     </Formik>
