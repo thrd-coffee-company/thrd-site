5d5742d4f5e0596db143867d50533863a0747c5b
Ozzy <ocardenas789@me.com>
2023-06-19 11:10:53 -0500

    add address to the footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,24 @@
-import { Box, Stack, Text } from "@chakra-ui/react";
-import { SocialIconsButtonGroup } from "../socialMediaIcons";
+import { Box, Stack, Text, Link } from '@chakra-ui/react';
+import { SocialIconsButtonGroup } from '../socialMediaIcons';
 
 const Footer = () => {
   return (
     <Box p={10} bg="purple.600">
       <Stack
         spacing={4}
-        direction={{ base: "column" }}
+        direction={{ base: 'column' }}
         align="center"
         color="white"
       >
         <SocialIconsButtonGroup />
         <Text as="span" textAlign="center">
           © {new Date().getFullYear()} Copyright: THRD COFFEE COMPANY, LLC
+        </Text>
+        <Text textAlign='center'>
+          <Link href="https://maps.google.com/?q=1816%20N%20Milwaukee%20Ave,%20Chicago,%20IL%2060647" isExternal>
+            1816 N Milwaukee Ave,
+            Chicago, IL 60647
+          </Link>
         </Text>
       </Stack>
     </Box>
